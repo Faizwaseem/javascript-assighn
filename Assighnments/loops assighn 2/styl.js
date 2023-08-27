6c957c0b422f17855ffd02a49ca5958fb8324b4d
Faizwaseem <136327727+Faizwaseem@users.noreply.github.com>
2023-08-27 13:35:06 -0700

    git update

---
+++ Assighnments/loops assighn 2/styl.js
@@ -0,0 +1,69 @@
+// const div = document.querySelector('div');
+// for (let i = 1; i <= 10; i++) {
+//     console.log(`bhar wala loop ${i}`);
+//     div.innerHTML += `<br/>`;
+//     for (let j = 1; j <= 10; j++) {
+//         console.log(`${i} * ${j} = ${i*j}`);
+//         div.innerHTML += `${i} * ${j} = ${i*j} <br/>`;
+//     }
+// }
+
+
+// Assighnment:01
+
+// const div = document.querySelector('div');
+
+// for (let a = 1; a <= 10; a++){
+
+// console.log (`bahar wala loop ${a}`);
+
+//     div.innerHTML += `<br/>`;
+
+// for (let b=1; b<=10; b++){
+
+// console.log(`${a} * ${b} = ${a+b}`);
+
+// div.innerHTML += `${a} *${b} =${a*b} <br/> `;
+
+
+// }
+// }
+
+
+
+
+const div=(document.querySelector('div'))
+
+for(let c=1; c<=100; c++){
+
+console.log ( `bahar wlaa loop ${c}`)
+
+div.innerHTML+= `<br/>`
+
+for(let d=1; d<=10; d++){
+
+    console.log(`${c}}*${d} =${c*d}`)
+
+div.innerHTML += `${c}*${d} =${c*d} <br/> `
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
