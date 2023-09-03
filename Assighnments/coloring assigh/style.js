a7ea51013c47e8a02d784b34a4e232ac822509c8
Faizwaseem <136327727+Faizwaseem@users.noreply.github.com>
2023-09-03 13:45:51 -0700

    update

---
+++ Assighnments/coloring assigh/style.js
@@ -0,0 +1,88 @@
+const div=document.querySelector('div')
+
+
+// div.innerHTML=`<li><button  onclick="button(red)">red</button> <button  onclick="button(yellow)">yellow</button> 
+// <button  onclick="button(green)">green</button>
+//  <button  onclick="button(black)">black</button> </li>`
+
+let colors=["purple","yellow",'green','black']
+function button(color){
+      if (color === "purple") {
+
+ document.body.style.backgroundColor = "purple";
+
+ }
+ 
+ else if (color === "green") {
+
+  document.body.style.backgroundColor = "green";
+ 
+  }
+  
+ else if (color === "blue") {
+
+    document.body.style.backgroundColor = "blue";
+   
+    }
+   else if (color === "brown") {
+
+      document.body.style.backgroundColor = "brown";
+     
+      }
+      
+     else if (color === "yellow") {
+
+        document.body.style.backgroundColor = "yellow";
+       
+        }
+        
+
+                 
+
+                }
+                 
+                 
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+                 
+
+
+
+
+
+
+
+
+
+                 
+              
+ 
+
+
+
+
