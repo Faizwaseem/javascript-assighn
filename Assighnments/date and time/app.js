8b6aabe49fd748bd5762f0cbbc2276592fe24fb0
Faizwaseem <136327727+Faizwaseem@users.noreply.github.com>
2023-09-07 12:54:09 -0700

    update

---
+++ Assighnments/date and time/app.js
@@ -0,0 +1,41 @@
+// let rightNow = new Date();
+// const days = ['sun', 'mon', 'tues', 'wed' , 'thur' , 'fri' , 'sat'];
+// console.log(rightNow.getDay())
+// console.log(days[rightNow.getDay()]);
+
+
+let faiz=new Date()
+
+const days=['sun','mon','tues','wed','thurs','frid','sat']
+
+
+console.log(faiz.getDay())
+console.log(days[faiz.getDay()])
+
+
+// console.log(faiz .getday())
+// console.log(days[faiz .getday()])
+
+
+
+
+
+
+
+
+
+
+
+
+
+// let rightNow = new Date('December 6,2022');
+// console.log('date' , rightNow);
+// console.log('date' , rightNow.getDate());
+// console.log('day' , rightNow.getDay());
+// console.log('hours' , rightNow.getHours());
+// console.log('minutes' , rightNow.getMinutes());
+// console.log('seconds' , rightNow.getSeconds());
+// console.log('millisecond' , rightNow.getMilliseconds());
+// console.log('month' , rightNow.getMonth());
+// console.log('full year' , rightNow.getFullYear());
+// console.log('time' , rightNow.getTime());
