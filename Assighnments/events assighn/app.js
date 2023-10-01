a32063c7e670fffbfb91ec585383a471ffa232f0
Faizwaseem <136327727+Faizwaseem@users.noreply.github.com>
2023-10-01 13:35:39 -0700

    update

---
+++ Assighnments/events assighn/app.js
@@ -0,0 +1,32 @@
+const img1 =document.querySelector('.img1')
+const img2 =document.querySelector('.img2')
+const div= document.querySelector('.container')
+
+img1.addEventListener('click', function(event){
+console.log('ulcalled')
+
+})
+
+
+
+
+
+
+
+// img1.addEventListener('click', function(event){
+    
+//     console.log('ul called')
+
+
+
+
+
+// })
+// img2.addEventListener('mouseout', function(e){
+// console.log('ulnot')
+
+
+
+// })
+
+
