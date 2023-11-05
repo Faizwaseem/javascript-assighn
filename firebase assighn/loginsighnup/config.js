239b4e1f022bbfd235bd2ab4e5592db9f2a7160e
Faizwaseem <136327727+Faizwaseem@users.noreply.github.com>
2023-11-05 13:13:56 -0800

    'update'

--- firebase assighn/loginsighnup/config.js
+++ firebase assighn/loginsighnup/config.js
@@ -1,21 +1,24 @@
 
-import { initializeApp } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app.js";
-// import { getAnalytics } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-analytics.js";
+  import { initializeApp } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-app.js";
 import { getAuth } from "https://www.gstatic.com/firebasejs/9.10.0/firebase-auth.js";
+// FireStore
+{/* <script src=""></script> */}
+import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-firestore.js";
+// import { getFirestore } from "https://www.gstatic.com/firebasejs/10.5.2/firebase-app-compat.js";
 
-const firebaseConfig = {
-  apiKey: "AIzaSyDjDSK4bHbpEt0nAbFobUcwkPBaieswTlI",
-  authDomain: "practice-cd827.firebaseapp.com",
-  projectId: "practice-cd827",
-  storageBucket: "practice-cd827.appspot.com",
-  messagingSenderId: "659462372837",
-  appId: "1:659462372837:web:070e914f1b124c5edd430c",
-  measurementId: "G-1JNM8KRQ1S"
-};
+  const firebaseConfig = {
+    apiKey: "AIzaSyBRjH6OC076MBJMo7tbAcoo4EuZpQPOcvM",
+    authDomain: "login-sighnup-5da68.firebaseapp.com",
+    projectId: "login-sighnup-5da68",
+    storageBucket: "login-sighnup-5da68.appspot.com",
+    messagingSenderId: "198769099859",
+    appId: "1:198769099859:web:cd33c868afec689f3332a6",
+    measurementId: "G-92HPPH2RDT"
+  };
 
-
-export  const app = initializeApp(firebaseConfig);
+  // Initialize Firebase
+  export const app = initializeApp(firebaseConfig);
 export const auth = getAuth(app);
-
+export const db = getFirestore(app);
 
 // const analytics = getAnalytics(app);
