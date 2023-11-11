92ba0f955762531649083354287d81b757990c8c
Faizwaseem <136327727+Faizwaseem@users.noreply.github.com>
2023-11-11 13:48:40 -0800

    'update'

---
+++ firebase assighn/loginsighnup/index.js
@@ -0,0 +1,38 @@
+
+import { db } from "./config";
+import { collection, addDoc} from "https://www.gstatic.com/firebasejs/9.10.0/firebase-firestore.js"; 
+
+
+
+const tittle=document.querySelector('#tittle')
+const description=document.querySelector('#description')
+const form=document.querySelector('form')
+const submit=document.querySelector('#sub')
+
+submit.addEventListener('click',async (e)=>{
+e.preventdefault()
+console.log(tittle.value)   
+console.log(description.value)   
+
+
+try {
+        const docRef = await addDoc(collection(db, "users"), {
+          tittle:tittle.value,
+          description:description.value,
+
+        });
+        console.log("Document written with ID: ", docRef.id);
+      } catch (e) {
+        console.error("Error adding document: ", e);
+      }
+    
+
+
+})
+
+
+
+
+
+
+
